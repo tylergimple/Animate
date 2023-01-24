418a71185b56a8c8ddfd2382c5e193945ae68948
tylergimple <59339453+tylergimple@users.noreply.github.com>
2023-01-24 11:10:17 -0500

    Update Animate.R

--- Animate.R
+++ Animate.R
@@ -1,17 +1,19 @@
 library(ggplot2)
-library(png) 
-library(gifski)
-library(gganimate)
 library(scales)
+library(gganimate)
 
+kk_data<-as.data.frame(kk_Trend)
+
+ggplot(kk_data, aes(AVG_Google_Trends, AVG_Kantar_Spend, color=Brand)) +
+  geom_point(size=1.75)+scale_y_continuous(labels = dollar)+labs(x = "Average Google Trend",y="Average Kantar Spend")
 
 
 p <- ggplot(kk_data, aes(AVG_Google_Trends, AVG_Kantar_Spend, color=Brand)) +
-  geom_point(size=1.75)+scale_y_continuous(labels = dollar)+labs(x = "Average Google Trend",y="Average Kantar Spend") +
+  geom_point(size=5)+scale_y_continuous(labels = dollar)+labs(x = "Average Google Trend",y="Average Kantar Spend") +
   transition_time(Month) +
-  shadow_mark()
+  shadow_mark(size = 1)
 
 animate(p,nframes = 320)
 
 
-anim_save(filename="animation2.gif", p)
+anim_save(filename="animation3.gif", p)
